253de11a4f21039ae8348336301fe7e6e613a991
florent.haffner <florent.haffner@protonmail.com>
2020-04-13 00:44:32 +0200

    model check the prediction percentage and return a correct value or an error message

--- main.py
+++ main.py
@@ -102,3 +102,38 @@ except:
     model.save("model.tflearn")
 
 
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return np.array(bag)
+
+def chat():
+    print("Start talking with the bot (type quit to stop) !")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])
+        for result in results:
+            results_index = np.argmax(result)
+            tag = labels[results_index]
+
+            if result[results_index] > 0.7:
+                for tg in data["intents"]:
+                    if tg['tags'] == tag:
+                        responses = tg['responses']
+                print(random.choice(responses))
+            else:
+                print("I didn't get that, try again.")
+
+chat()
+
