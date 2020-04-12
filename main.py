da38532ac86cc9d2e923b2f4faeb277dd7ee4b46
florent.haffner <florent.haffner@protonmail.com>
2020-04-12 23:10:34 +0200

    pushed data to NN

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import nltk
+nltk.download('punkt')
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
+import numpy as np
 import tflearn
 import tensorflow
 import random
@@ -11,5 +12,74 @@ import json
 with open("intents.json") as file:
     data = json.load(file)
 
-print(data["intents"])
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+#
+# Getting all the shit out of our data
+#
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tags"])
+
+    if intent["tags"] not in labels:
+        labels.append(intent["tags"])
+
+
+#
+# Removing dupplicate element and stuff related
+#
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+out_empty = [0 for _ in range(len(labels))]
+
+#
+# ENCODER
+#
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+# Turned our list into np array to be able to work with TF
+training = np.array(training)
+output = np.array(output)
+
+# Reset then set NN configuration
+tensorflow.reset_default_graph()
+
+# Neurol Network configuration
+# Input = 45
+# Hidden #1 = 8
+# Hidden #2 = 8
+# Output = 6 -> softmax activation function (prediction on the most highly predicted output)
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 
